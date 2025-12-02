4c420c1e5493aea4d553933546c07ba3cffe3a97
Ryan <yujuism@gmail.com>
2025-12-02 15:51:17 +0700

    afl-2 init

---
+++ app/Models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'slug'
+    ];
+
+    /**
+     * Get products that belong to this category
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    /**
+     * Get the route key for the model.
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
