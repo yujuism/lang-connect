ac54a88cac8e8a14bbf29477bf4ff5bb36f57791
Ryan <yujuism@gmail.com>
2026-01-10 01:23:10 +0700

    Fix: Include app.css in layout.blade.php for tldraw CSS fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -127,7 +127,7 @@
     @yield('extra-css')
 
     <!-- Vite Assets (includes WebSocket / Laravel Echo) -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <!-- Navigation -->
